64e1ad5966c615865f950256676547441baf992e
mateusseiboth <mateusseiboth@gmail.com>
2023-04-21 19:56:27 -0400

    Upload inicial

---
+++ resources/views/template.blade.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mateus Seiboth and Flávio">
+    <title>Carrinhos maravilhosos v0.3</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/navbar-fixed/">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <style>
+        .bi {
+            vertical-align: -0.125em;
+            fill: currentColor;
+        }
+    </style>
+</head>
+
+
+<body onload="carregarTema()">
+    <div class="bg-dark text-white d-flex" style="height: 100%">
+        <div class="d-flex flex-column flex-shrink-0 p-2 text-white bg-primary"
+            style="width: 13%;">
+            <div class="row text-center">
+                <a href="/"
+                    class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <i class="bi me-4 bi-house-fill" style="font-size: 40px">
+                        <span class="fme-4 s-4">Cutucar</span>
+                    </i>
+                </a>
+            </div>
+            <hr>
+            <ul class="nav nav-pills flex-column mb-auto">
+                <li class="nav-item ">
+                    <a href="#" class="nav-link text-white" aria-current="page">
+                        <i class="me-2 bi bi-wrench-adjustable" style="font-size: 20px"></i>
+                        Painel Admin.
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-p-circle" style="font-size: 20px"></i>
+                        Vagas
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-car-front" style="font-size: 20px"></i>
+                        Carros
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-person" style="font-size: 20px"></i>
+                        Clientes
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-ticket-perforated" style="font-size: 20px"></i>
+                        Tickets Ativos
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-ticket-detailed" style="font-size: 20px"></i>
+                        Todos os tickets
+                    </a>
+                </li>
+                <li>
+                    <a href="#" class="nav-link text-white">
+                        <i class="me-2 bi bi-person-badge" style="font-size: 20px"></i>
+                        Cadastrar Usuário
+                    </a>
+                </li>
+            </ul>
+            <hr>
+            <div class="dropdown">
+                <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle"
+                    id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="" alt="" width="32" height="32" class="rounded-circle me-2">
+                    <strong>mdo</strong>
+                </a>
+                <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                    <li><a class="dropdown-item" href="#">New project...</a></li>
+                    <li><a class="dropdown-item" href="#">Settings</a></li>
+                    <li><a class="dropdown-item" href="#">Profile</a></li>
+                    <li>
+                        <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Sign out</a></li>
+                </ul>
+            </div>
+        </div>
+
+        <main class="container-fluid mb-1">
+            <div class="p-5 border border-primary container mt-4" id="border-main">
+                @yield('conteudo')
+            </div>
+        </main>
+    </div>
+
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3">
+          <p class="col-md-4 mb-0 text-muted">© 2023 CutuCar, Inc</p>
+
+          <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted"><i class="bi bi-github"></i>
+            </a></li>
+          </ul>
+        </footer>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.min.js"
+        integrity="sha384-mQ93GR66B00ZXjt0YO5KlohRA5SY2XofN4zfuZxLkoj1gXtW8ANNCe9d5Y3eG5eD" crossorigin="anonymous">
+    </script>
+
+</body>
+
+
+</html>
