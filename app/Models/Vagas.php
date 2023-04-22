18a1a5c0d37dc8b512ea2db34c876e525bdc41b4
mateusseiboth <mateusseiboth@gmail.com>
2023-04-21 21:10:59 -0400

    Criado vaga model

---
+++ app/Models/Vagas.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Vagas extends Model
+{
+    use HasFactory;
+    protected $table = 'vaga';
+    public $timestamps = false;
+}
