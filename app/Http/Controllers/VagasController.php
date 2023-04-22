53eda96312e878de67c1151f6075d81e0fca8f9c
mateusseiboth <mateusseiboth@gmail.com>
2023-04-21 21:10:50 -0400

    Adição de função de listagem

--- app/Http/Controllers/VagasController.php
+++ app/Http/Controllers/VagasController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Vagas;
 
 class VagasController extends Controller
 {
-    //
+    function listar() {
+        $vagas = Vagas::orderBy('id')->get();
+        return view('vagas',
+                      compact('vagas'));
+      }
 }
