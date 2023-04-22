9bd09d0082df28da603deb38e5d6b0404eed0ef7
mateusseiboth <mateusseiboth@gmail.com>
2023-04-22 09:26:32 -0400

    Alteração da databse (preparação para login)

--- config/database.php
+++ config/database.php
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'estaciona'),
+            'database' => env('DB_DATABASE', 'estaciona-laravel'),
             'username' => env('DB_USERNAME', 'postgres'),
             'password' => env('DB_PASSWORD', 'postgres'),
             'charset' => 'utf8',
