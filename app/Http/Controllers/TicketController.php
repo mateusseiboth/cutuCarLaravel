955db06e0c8726c1a4e72f5e0c00c4a931789cb5
mateusseiboth <mateusseiboth@gmail.com>
2023-04-22 09:25:23 -0400

    Criada função de listagem de ativos e todos

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -3,8 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Ticket;
 
 class TicketController extends Controller
 {
-    //
+    function listarAtivos(){
+        $page = ['title' => 'Tickets Ativos', 'botao' => true];
+        $tickets = Ticket::where('estado', true)->orderByRaw('id')->get();
+        return view('tickets',
+                      compact('tickets', 'page'));
+    }
+
+    function listarTodos(){
+        $page = ['title' => 'Todos os Tickets', 'botao' => false];
+        $tickets = Ticket::where('estado', false)->orderByRaw('id')->get();
+        return view('tickets',
+                      compact('tickets','page'));
+    }
 }
