3391b788e7db3110c89ba76649df1ed8d845febe
mateusseiboth <mateusseiboth@gmail.com>
2023-04-22 09:25:10 -0400

    Criada função de listagem

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Carro;
 
 class CarroController extends Controller
 {
-    //
+    function listar(){
+        $carros = Carro::orderByRaw('id')->get();
+        return view('carros',
+                      compact('carros'));
+    }
 }
