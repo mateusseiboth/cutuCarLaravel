ab461d4df24076c733323daf2daf72f3c8c6fb9c
mateusseiboth <mateusseiboth@gmail.com>
2023-04-24 10:46:41 -0400

    Correção na modal

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -5,20 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Ticket;
+use App\Models\Carro;
+use App\Models\Tipo;
+use App\Models\Vagas;
 
 class TicketController extends Controller
 {
     function listarAtivos(){
         $page = ['title' => 'Tickets Ativos', 'botao' => true];
         $tickets = Ticket::where('estado', true)->orderByRaw('id')->get();
+        $carros = Carro::orderBy('id')->get();
+        $tipos = Tipo::orderBy('id')->get();
+        $vagas = Vagas::orderBy('id')->get();
         return view('tickets',
-                      compact('tickets', 'page'));
+                      compact('tickets', 'page', 'carros', 'tipos', 'vagas'));
     }
 
     function listarTodos(){
         $page = ['title' => 'Todos os Tickets', 'botao' => false];
         $tickets = Ticket::where('estado', false)->orderByRaw('id')->get();
+        $carros = Carro::orderBy('id')->get();
+        $tipos = Tipo::orderBy('id')->get();
+        $vagas = Vagas::where('estado', true)->orderByRaw('id')->get();
         return view('tickets',
-                      compact('tickets','page'));
+                      compact('tickets', 'page', 'carros', 'tipos', 'vagas'));
     }
 }
--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -9,11 +9,15 @@
         border-left: 1px dashed black;
         /* borda esquerda pontilhada */
     }
+
+    .nafrente {
+        z-index: 99999;
+    }
 </style>
 @section('conteudo')
     <h1 class="text-center">
         <i class="bi bi-ticket" style="font-size: 3rem"></i>
-        <div class="text-center">{{$page['title']}}</div>
+        <div class="text-center">{{ $page['title'] }}</div>
     </h1>
     <div class='mb-3'>
         <button type="button" class="btn btn-primary col-md-2" data-bs-toggle="modal" data-bs-target="#myModal">
@@ -71,11 +75,12 @@
                     </div>
 
                     @php
-                    $botao = $page['botao'] ? '' : 'd-none';
+                        $botao = $page['botao'] ? '' : 'd-none';
                     @endphp
-                    <div class="{{$botao}} card-footer border-0 bg-light p-2 d-flex justify-content-around">
-                        <a href='' class='{{$botao}} btn btn-link m-0 bg-danger text-reset text-decoration-none' role="button"
-                            data-ripple-color="danger"> <i class="bi bi-trash"></i>
+                    <div class="{{ $botao }} card-footer border-0 bg-light p-2 d-flex justify-content-around">
+                        <a href=''
+                            class='{{ $botao }} btn btn-link m-0 bg-danger text-reset text-decoration-none'
+                            role="button" data-ripple-color="danger"> <i class="bi bi-trash"></i>
                             Encerrar
                         </a>
                     </div>
@@ -83,14 +88,12 @@
             </div>
         @endforeach
     </div>
-
-    </div>
     <!-- Criação da modal -->
-    <div class="modal fade" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content corzinha">
+    <div class="modal fade text-black" data-bs-backdrop="false" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog nafrente">
+            <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Inserir/Editar Carro</h5>
+                    <h5 class="modal-title text-dark" id="exampleModalLabel">Inserir Ticket</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Fechar"></button>
                 </div>
                 <div class="modal-body">
@@ -101,35 +104,53 @@
                         <input type="hidden" name="id" value="">
 
                         <div class="mb-3">
-                            <label for="nome" class="form-label">Nome:</label>
+
+                            <label for="placa" class="form-label">Carro:</label>
                             <div class="input-group col-mb-3">
                                 <span class="input-group-text" id="basic-addon1">
-                                    <i class="bi bi-spellcheck"></i>
+                                    <i class="bi bi-car-front"></i>
                                 </span>
-                                <input type="text" name="nome" id="nome" class="form-control">
-                                <small id="msgNome" class="form-text text-danger"></small>
+                                <select name="carro_id" class="form-select" aria-label="cliente_id">
+                                    <option selected name='cliente_id' value='-1'>Selecione um carro</option>
+                                    @foreach ($carros as $carro)
+                                        <option name='carro_id' value='{{ $carro->id }}'>{{ $carro->placa }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
                         <div class="mb-3">
-                            <label for="cpf" class="form-label">CPF:</label>
+
+                            <label for="tipo_id" class="form-label">Tipo de estadia</label>
                             <div class="input-group col-mb-3">
                                 <span class="input-group-text" id="basic-addon1">
-                                    <i class="bi bi-person-vcard"></i>
+                                    <i class="bi bi-clock"></i>
                                 </span>
-                                <input type="text" name="cpf" id="cpf" class="form-control">
-                                <small id="msgCpf" class="form-text text-danger"></small>
+                                <select name="tipo_id" class="form-select" aria-label="tipo_id">
+                                    <option selected name='tipo_id' value='-1'>Selecione um tipo</option>
+                                    @foreach ($tipos as $tipo)
+                                        <option name='tipo_id' value='{{ $tipo->id }}'>{{ $tipo->descr }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
                         <div class="mb-3">
-                            <label for="telefone" class="form-label">Telefone:</label>
+
+                            <label for="tipo_id" class="form-label">Selecione a Vaga</label>
                             <div class="input-group col-mb-3">
                                 <span class="input-group-text" id="basic-addon1">
-                                    <i class="bi bi-telephone"></i>
+                                    <i class="bi bi-p-circle"></i>
                                 </span>
-                                <input type="text" name="telefone" id="telefone" class="form-control">
-                                <small id="msgTelefone" class="form-text text-danger"></small>
+                                <select name="tipo_id" class="form-select" aria-label="tipo_id">
+                                    <option selected name='tipo_id' value='-1'>Selecione uma vaga</option>
+                                    @foreach ($vagas as $vaga)
+                                        <option name='tipo_id' value='{{ $vaga->id }}'>{{ $vaga->id }}
+                                        </option>
+                                    @endforeach
+                                </select>
                             </div>
                         </div>
 
