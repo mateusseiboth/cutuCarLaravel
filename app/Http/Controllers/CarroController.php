7e73fca0e5c95866e9536ec7bcddbc8bae441027
mateusseiboth <mateusseiboth@gmail.com>
2023-04-24 10:45:56 -0400

    Adição de listagem de clientes (modal)

--- app/Http/Controllers/CarroController.php
+++ app/Http/Controllers/CarroController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Models\Carro;
+use App\Models\Cliente;
 
 class CarroController extends Controller
 {
     function listar(){
         $carros = Carro::orderByRaw('id')->get();
+        $clientes = Cliente::orderByRaw('id')->get();
         return view('carros',
-                      compact('carros'));
+                      compact('carros', 'clientes'));
     }
 }
