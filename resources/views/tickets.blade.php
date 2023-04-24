b0cdb25dec937bcc0cb8764c12d47b06f3e93cb3
mateusseiboth <mateusseiboth@gmail.com>
2023-04-24 11:12:06 -0400

    Criada função para cadstro direto do veiculo

--- resources/views/tickets.blade.php
+++ resources/views/tickets.blade.php
@@ -89,7 +89,8 @@
         @endforeach
     </div>
     <!-- Criação da modal -->
-    <div class="modal fade text-black" data-bs-backdrop="false" id="myModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade text-black" data-bs-backdrop="false" id="myModal" tabindex="-1"
+        aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog nafrente">
             <div class="modal-content">
                 <div class="modal-header">
@@ -110,13 +111,19 @@
                                 <span class="input-group-text" id="basic-addon1">
                                     <i class="bi bi-car-front"></i>
                                 </span>
-                                <select name="carro_id" class="form-select" aria-label="cliente_id">
-                                    <option selected name='cliente_id' value='-1'>Selecione um carro</option>
+                                <select name="carro_id" class="form-select" aria-label="carro_id">
+                                    <option selected name='carro_id' value='-1'>Selecione um carro</option>
                                     @foreach ($carros as $carro)
                                         <option name='carro_id' value='{{ $carro->id }}'>{{ $carro->placa }}
                                         </option>
                                     @endforeach
                                 </select>
+                            </div>
+                            <div class="form-check mt-2 mb-2 ms-1">
+                                <input name="cadastrado" class="form-check-input" type="checkbox" id="placaCheck">
+                                <label class="form-check-label text-muted" for="placaCheck">
+                                    Sem cadastro?
+                                </label>
                             </div>
                         </div>
 
@@ -164,4 +171,18 @@
             </div>
         </div>
     </div>
+
+
+    <script>
+        document.getElementById("placaCheck").addEventListener("click", function() {
+            var select = document.querySelector("select[name='carro_id']");
+            var input = document.createElement("input");
+            input.setAttribute("type", "text");
+            input.setAttribute("name", "placa");
+            input.setAttribute("class", "form-control");
+            input.setAttribute("placeholder", "Placa do carro");
+            input.value = select.options[select.selectedIndex].text;
+            select.parentNode.replaceChild(input, select);
+        });
+    </script>
 @endsection
