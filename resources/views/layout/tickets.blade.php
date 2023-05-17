317202bb1ff7fe1539c2b17382e70d95dea33f93
mateusseiboth <mateusseiboth@gmail.com>
2023-05-17 19:48:58 -0400

    Adição da função de clique no botão de encerrar

--- resources/views/layout/tickets.blade.php
+++ resources/views/layout/tickets.blade.php
@@ -75,13 +75,15 @@
                     @php
                         $botao = $page['botao'] ? '' : 'd-none';
                     @endphp
+
                     <div class="{{ $botao }} card-footer border-0 bg-light p-2 d-flex justify-content-around">
-                        <a href=''
-                            class='{{ $botao }} btn btn-link m-0 bg-danger text-reset text-decoration-none'
-                            role="button" data-ripple-color="danger">
-                            <i class="fa-sharp fa-solid fa-trash text-white"></i>
-                            <span class="text-white" style="font-weight: bold;">Encerrar</span>
-                        </a>
+
+                            <a href='/tickets/{{ $ticket->id }}'
+                                class='{{ $botao }} btn btn-link m-0 bg-danger text-reset text-decoration-none'
+                                role="button" data-ripple-color="danger">
+                                <i class="fa-sharp fa-solid fa-trash text-white"></i>
+                                <span class="text-white" style="font-weight: bold;">Encerrar</span>
+                            </a>
                     </div>
                 </div>
             </div>
@@ -92,6 +94,6 @@
     <div class="d-flex justify-content-center" style="padding-top: 2em">
         {{ $tickets->links() }}
     </div>
-    
+
     @include('modals.ticketsModal')
 @endsection
